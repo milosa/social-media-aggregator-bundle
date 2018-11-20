d614620b410c451d69451c4c6078e8d54d00f78e
sander <sander3@gmail.com>
2018-11-20 17:15:10 +0100

    Move fetcher

--- Aggregator/Fetcher.php
+++ Aggregator/Fetcher.php
@@ -2,9 +2,8 @@
 
 declare(strict_types=1);
 
-namespace Milosa\SocialMediaAggregatorBundle\Aggregator\Sites;
+namespace Milosa\SocialMediaAggregatorBundle\Aggregator;
 
-use Milosa\SocialMediaAggregatorBundle\Aggregator\Message;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 abstract class Fetcher
